db990ea8dff74675566d2bb70b677f06c8015718
Henry Kupty <hkupty@gmail.com>
2018-12-10 13:30:29 +0100

    Don't bail out if neovim errors
    This also removes the formatting from neovim layer

--- src/main/scala/nero/Main.scala
+++ src/main/scala/nero/Main.scala
@@ -1,13 +1,32 @@
 package nero
 
 import scala.util.control.TailCalls._
+import scala.util.control.NoStackTrace
 import java.io.{PrintStream, OutputStream, InputStream}
 import scala.io.StdIn
 import java.net.Socket
 
 
-trait REPL {
+object REPL {
   case class ReplState(nvim: Neovim, currentMode: Lang)
+  case class NeroError(val message: String) extends NoStackTrace with ReplResponse
+
+  def replToString(msg: ReplResponse): String = msg match {
+    case NeovimNop => "[NOP ]"
+    case NeovimMessage(msg) => s"[OK  ] $msg"
+    case NeovimError(msg) => s"[FAIL] $msg"
+    case NeroError(msg) => s"[ERR ] $msg"
+  }
+  val ps: PrintStream = new PrintStream(System.out)
+  def out(msg: ReplResponse) = ps.println(replToString(msg))
+
+  def reload(pkg: String): String = s"""
+  | for pkg, _ in pairs(package.loaded) do
+  |   if pkg:sub(${pkg.size}) == "${pkg}" then
+  |     package.loaded[pkg] = nil
+  |   end
+  | end""".stripMargin('|')
+
 
   def handleCommands(state: ReplState, args: String*): TailRec[ReplState] = {
     val cmd :: arguments = args.toList
@@ -15,6 +34,11 @@ trait REPL {
 
     cmd match {
       case x if Set(":q", ":q!", ":quit", ":quit!", ":wq", ":x:") contains x => done(state)
+      case ":reload" => {
+        val pkg :: _ = arguments
+        out(state.nvim.send(Lua, reload(pkg)))
+        recur(state)
+      }
       case ":set" => {
         val lang :: _ = arguments
         lang match {
@@ -23,13 +47,12 @@ trait REPL {
         }
       }
       case x => {
-        ps.println(s"[Err ]   '$x' is not a repl command")
+        out(NeroError(s"'${x}' is not a repl command"))
         recur(state)
       }
     }
   }
 
-  val ps: PrintStream = new PrintStream(System.out)
 
   final def repl(state: ReplState): TailRec[ReplState] = {
     val prompt = state.currentMode match {
@@ -40,20 +63,21 @@ trait REPL {
     StdIn.readLine(prompt) match {
       case cmd if cmd.startsWith(":") => handleCommands(state, cmd.split(" "):_*)
       case cmd => tailcall{
-        val resp = state.nvim.send(state.currentMode, cmd)
-        ps.println(resp)
+        out(state.nvim.send(state.currentMode, cmd))
         repl(state)
       }
     }
   }
 
+  def run(nvim: Neovim, currentMode: Lang = Lua): ReplState = repl(ReplState(nvim, currentMode)).result
+
 }
 
-object Main extends REPL {
+object Main {
   def main(args: Array[String]): Unit = {
     val nvim = Neovim.fromSocket(new Socket("127.0.0.1", 12345))
 
-    repl(ReplState(nvim, Lua)).result
+    REPL.run(nvim)
 
     nvim.close()
   }
